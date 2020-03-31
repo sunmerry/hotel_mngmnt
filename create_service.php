8e7f7e275b721a55bbcda303da0da3cf46ba3424
Surya Raj <suryaraj242@gmail.com>
2020-03-31 12:39:05 +0530

    Add files via upload

--- create_service.php
+++ create_service.php
@@ -20,6 +20,8 @@ else
 {
 			
 			$sql = "INSERT INTO service (service_name, service_cost) VALUE ('$service_name', $service_cost)";
+			$query="SELECT * FROM service";
+			$result=mysqli_query($conn,$query);		
 		
 		if(mysqli_query($conn, $sql)){
 			echo "<h1>new service record inserted suceessfully</h1>";
@@ -27,7 +29,35 @@ else
 		else{
 			echo "error"." ". mysqli_error($conn);
 		}
-		mysqli_close($conn);
+?>
+		<html>
+		
+		<body>
+		<table>
+			<tr>
+				<th>Service details</th>
+				
+			</tr>
+			<t>
+				<th>service id</th>
+				<th>service name</th>
+				<th>service cost</th>
+			</t
+			<?php
+			while($rows=mysqli_fetch_assoc($result))
+			{
+				?>
+				<tr>
+					<th><?php echo $rows['service_id'] ; ?></th>
+					<th><?php echo $rows['service_name'] ; ?>;</th>
+					<th><?php echo $rows['service_cost'] ; ?></th>
+				</tr>
+			
+		<?php	} ?>
+		</table>
+		</body>
+		</html>
+	<?php	mysqli_close($conn);
 }
 
 
